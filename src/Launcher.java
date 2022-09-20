66b1d677594510321cd97e2c66f0047ce419e0bb
Antoine ADAM <flancraft10@gmail.com>
2022-09-20 15:00:43 +0200

    Partie 4 - une première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,16 +1,37 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibo(int n){
+        if(n<=1){
+            return n;
+        }else {
+            return fibo(n-1) + fibo(n-2);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("bienvenue");
         Scanner scanner = new Scanner(System.in);
+        String response;
         while (true){
             System.out.print("command:");
-            String response = scanner.nextLine();
-            if(!response.equals("quit")){
-                System.out.println("Unknown command");
-            }else {
-                return;
+            response = scanner.nextLine().toLowerCase();
+            switch (response){
+                case "quit":
+                    return;
+                case "fibo":
+                    System.out.print("n=");
+                    if(scanner.hasNextInt()){
+                        int n = scanner.nextInt();
+                        scanner.nextLine();
+                        int res = Launcher.fibo(n);
+                        System.out.println(res);
+                    }else
+                        scanner.nextLine();
+                    break;
+                default:
+                    System.out.println("Unknown command");
+                    break;
             }
         }
     }
